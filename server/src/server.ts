4ba9e2317b12810a2315c9105f2b8abc1768fd2a
Daniel Rocha <dgustavo@id.uff.br>
2023-05-17 21:53:14 -0300

    server/feat: add memories CRUD

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,14 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
-
-app.get('/hello', () => {
-  return 'Hello World'
-})
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('Server running on http://localhost:3333')
+    console.log('🚀 HTTP server running on port http://localhost:3333')
   })
