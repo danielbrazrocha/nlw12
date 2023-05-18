dcdae83ab08430e6c3c8592131db3e4bb5c21304
Daniel Rocha <dgustavo@id.uff.br>
2023-05-18 19:49:46 -0300

    web/feat: add initial page to receive exchange code

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 
 import {
   useFonts,
@@ -13,16 +15,44 @@ import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
-import React from 'react'
-
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/cec449a3cb1d648b1541',
+}
+
 export default function App() {
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: 'cec449a3cb1d648b1541',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    // console utilizado para verificar o callbackURL chamado e inserir o link no github oauth app
+    // console.log(makeRedirectUri({
+    //   scheme: 'nlwspacetime',
+    // }))
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      console.log("🚀 ~ file: App.tsx:49 ~ useEffect ~ code:", code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -51,6 +81,7 @@ export default function App() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={() => signInWithGithub()}
           className="rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black">
