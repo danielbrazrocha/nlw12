8def798898147872d2f35d390e4af29405eab6ac
Daniel Rocha <dgustavo@id.uff.br>
2023-05-19 00:08:04 -0300

    web/feat: add layout, routing and new memory page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,22 +1,10 @@
 import { useEffect } from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
 import { api } from '../src/lib/api'
 import * as SecureStore from 'expo-secure-store'
 import { useRouter } from 'expo-router'
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -27,12 +15,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -51,7 +33,10 @@ export default function App() {
     })
 
     const { token } = response.data
-    console.log("🚀 ~ file: index.tsx:54 ~ handleGithubOAuthCode ~ token:", token)
+    console.log(
+      '🚀 ~ file: index.tsx:54 ~ handleGithubOAuthCode ~ token:',
+      token,
+    )
 
     await SecureStore.setItemAsync('token', token)
 
@@ -65,23 +50,13 @@ export default function App() {
     // }))
     if (response?.type === 'success') {
       const { code } = response.params
-      console.log("🚀 ~ file: App.tsx:49 ~ useEffect ~ code:", code)
+      console.log('🚀 ~ file: App.tsx:49 ~ useEffect ~ code:', code)
       handleGithubOAuthCode(code)
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -97,8 +72,8 @@ export default function App() {
 
         <TouchableOpacity
           activeOpacity={0.7}
-          onPress={() => signInWithGithub()}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
@@ -109,8 +84,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
